f9868f70152b550f060b3d053aaa427804f30f89
Sitaram <sitaram_chikkala@apple.com>
2020-08-04 23:01:21 +0530

    intial commit

---
+++ easyregex/EasyRegex.py
@@ -0,0 +1,23 @@
+import regex
+
+
+
+
+
+class EasyRegex:
+
+    def numbers(self, length):
+        print()
+
+    def alphabets(self, length):
+        print()
+
+    def starts_with(self, string):
+        print()
+
+    def ends_with(self, string):
+        print()
+
+
+
+
